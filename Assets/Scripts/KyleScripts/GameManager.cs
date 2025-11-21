7b92bc5b0da281c1f041bf024211146f2993d741
kylelirette93 <162719550+kylelirette93@users.noreply.github.com>
2025-11-21 14:41:57 -0400

    Created game manager.
    Using singleton pattern for service locator.

---
+++ Assets/Scripts/KyleScripts/GameManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    private void Awake()
+    {
+        #region Singleton
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        #endregion
+    }
+}
