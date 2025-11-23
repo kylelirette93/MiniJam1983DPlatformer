fcf816003e677ff5fd53eaf626b513d98288ef6a
kylelirette93 <162719550+kylelirette93@users.noreply.github.com>
2025-11-23 18:00:01 -0400

    Added particle system for collectibles

--- Assets/Scripts/HunterScripts/PickupTrigger.cs
+++ Assets/Scripts/HunterScripts/PickupTrigger.cs
@@ -2,14 +2,18 @@ using UnityEngine;
 
 public class PickupTrigger : MonoBehaviour
 {
+    [SerializeField] ParticleSystem particles;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            Vector3 spawnPosition = transform.position;
             // This is where we do stuff.
             // Pickup recharges stamina
             GameManager.Instance.AudioManager.PlaySFX("Pickup", 1);
             Stamina.Increase(15f);
+            GameObject temp = Instantiate(particles.gameObject, spawnPosition, Quaternion.identity);
             Destroy(gameObject);
         }
     }
